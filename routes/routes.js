3ad7eb8921e75e0bd7c48e9b01c2d630b6b66bd5
anidzarts <76755210+anidzarts@users.noreply.github.com>
2021-01-07 03:43:50 +0200

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import * as readersController from "./controllers/readersController.js";
 import * as readersApi from "./apis/readersApi.js";
 import * as cardsController from "./controllers/cardsController.js";
 import * as cardsApi from "./apis/cardsApi.js";
+import * as keysApi from "./apis/keysApi.js";
 
 const router = new Router();
 
@@ -19,5 +20,14 @@ router.get('/readers/delete/:id', readersApi.deleteReadersItem);
 
 router.get('/cards/:id', cardsController.cardsItem);
 router.get('/cards/delete/:id', cardsApi.deleteCardsItem);
+
+
+router.get('/auth-key:id', keysApi.keysAuth);
+router.get('/signing-key:id', keysApi.keysSign);
+router.post('/block-list', keysApi.blockCard);
+router.get('/cards/block:id', keysApi.cardsBlock);
+router.get('/readers/block:id', keysApi.readersBlock);
+
+router.get('/change/block:name', keysApi.changeBlock);
 
 export { router };
